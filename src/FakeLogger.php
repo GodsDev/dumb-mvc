213579d6340fd668d94cb39b1014867a680db2e1
tomaskraus <tomas.krausik@gmail.com>
2016-09-19 23:13:13 +0200

    working basics, composer file

---
+++ src/FakeLogger.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Dmc\Utils;
+
+/**
+ * just to mock log4php basic logger behavior
+ * for internal purpose
+ *
+ * @author Tomáš
+ */
+class FakeLogger {
+
+    public function debug($message, Exception $throwable = null ) {
+
+    }
+    public function info($message, Exception $throwable = null ) {
+
+    }
+    public function error($message, Exception $throwable = null ) {
+
+    }
+}
