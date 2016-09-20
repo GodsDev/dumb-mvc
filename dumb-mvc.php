572d5c8bca74c6b1e767096bf10adfe62430c4c8
tomaskraus <tomas.krausik@gmail.com>
2016-09-20 20:15:12 +0200

    fix: path, instance

--- dumb-mvc.php
+++ dumb-mvc.php
@@ -5,21 +5,19 @@
  *
  * @author Tomáš
  */
-require_once __DIR__ . "/vendor/tomaskraus/php-includer/src/PI.php";
+require_once __DIR__ . "/../php-includer/src/PI.php";
 require_once __DIR__ . "/src/FakeLogger.php";
 require_once __DIR__ . "/src/DumbMVC.php";
 
 \DumbMVC\DumbMVC::setInstance(
         new \DumbMVC\DumbMVC(
-        new \PhpIncluder\PI(__DIR__ . "/../../../"), new \FakeLogger()
+        new \PhpIncluder\PI(__DIR__ . "/../../../"), new \DumbMVC\FakeLogger()
         )
 );
 
-
 //include file in app root dir
-$bootstrapFilename = $dmc->fullPath("dmc.bootstrap.php");
+$bootstrapFilename = \DumbMVC\DumbMVC::instance()->fullPath("dmc.bootstrap.php");
 if (file_exists($bootstrapFilename)) {
-    logger()->debug("including bootstrap file [$bootstrapFilename]");
+    \DumbMVC\DumbMVC::instance()->contextLogger()->debug("including bootstrap file [$bootstrapFilename]");
     include_once $bootstrapFilename;
 }
-
